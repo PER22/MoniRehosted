1ed73d95cdef02f935521536065e1ea10de4f125
hayden Pour <houshmand.esmaeilpour@gmail.com>
2020-10-10 16:32:10 -0700

    added mvc directories

--- Frontend/main.js
+++ Frontend/main.js
@@ -11,7 +11,7 @@ function createWindow () {
   })
 
   // and load the index.html of the app.
-  win.loadFile('src/index.html')
+  win.loadFile('src/View/index.html')
 
   // Open the DevTools.
   win.webContents.openDevTools()
