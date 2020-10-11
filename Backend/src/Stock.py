1221fa61faf1b68ea0bae0ce144f4d59b8bb0a70
Julian Beaulieu <32250690+JulianBeaulieu@users.noreply.github.com>
2020-10-10 23:40:04 -0700

    Update from master

--- Backend/src/Stock.py
+++ Backend/src/Stock.py
@@ -1,17 +1,25 @@
 class Stock:
+    name = ""
     label = ""
-    data = []
+    data = [] #[["date" "Open" "High" "Low" "Close" "Volume"]]
 
 
-    def __init__(self, new_label):
-        self.label = new_label
+    def __init__(self, new_name = None, new_label = None, new_data = None):
+        if new_name is not None:
+            self.name = new_name
+
+        if new_data is not None:
+            self.data = new_data
+
+        if new_label is not None:
+            self.label = new_label
 
     def toString(self):
         return("Name: " + self.name + " | Label: " + self.label + " | Data: " + str(self.data))
 
     def toJSON(self):
         return{
-                "name": self.name,
-                "name": self.label,
-                "data": self.data
-              }
+            "name": self.name,
+            "name": self.label,
+            "data": self.data
+        }
