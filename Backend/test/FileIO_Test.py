15dc261acc9c3069d14716cecd3b4c7c85df19c3
Julian Beaulieu <32250690+JulianBeaulieu@users.noreply.github.com>
2020-10-10 23:50:35 -0700

    Created Unit test file and made stubs

---
+++ Backend/test/FileIO_Test.py
@@ -0,0 +1,14 @@
+from unittest import TestCase
+from JSON_Converter import JSON_Converter
+from Stock import Stock
+
+class JSON_Converter_Test(TestCase):
+
+    def test_load(self):
+        self.assertEqual(True, True)
+
+    def test_save(self):
+        self.assertEqual(True, True)
+
+
+    #-=-=-=- Helper Function -=-=-=-=-=-
