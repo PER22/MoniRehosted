cbfca97f897158f58e729b5887d7f9e544d9784d
Julian Beaulieu <32250690+JulianBeaulieu@users.noreply.github.com>
2020-10-26 12:21:40 -0700

    Chages to filepath

--- Backend/src/Model/Database.py
+++ Backend/src/Model/Database.py
@@ -1,4 +1,7 @@
-from Backend.src.Model.Stock import Stock
+import sys
+sys.path.insert(1, '../')
+
+from Model.Stock import Stock
 import requests
 from threading import Thread
 import csv
@@ -20,7 +23,7 @@ class Database:
 
         #go to correct file location to see the different companies.
         dirname = (os.path.dirname(__file__))[:-9]
-        data_directory = os.path.join(dirname, 'data') + os.sep
+        data_directory = os.path.join(dirname, '../../data') + os.sep
 
         print(data_directory)
         #Stocks directory
